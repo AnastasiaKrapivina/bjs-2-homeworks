4a4e5533f26c0aab407a917cefe92421c5d0ccef
Anastasia Krapivina <krapivina934@gmail.com>
2023-09-02 22:30:57 +0700

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,8 @@
-function compareArrays(arr1, arr2) {
-  
-}
+const compareArrays = (arr1, arr2) => arr1.length === arr2.length && arr1.every((item, i) => item === arr2[i]);
 
-function getUsersNamesInAgeRange(users, gender) {
+const getUsersNamesInAgeRange = (users, gender) => {
+  const selectedUsers = users.filter((user) => user.gender === gender);
+  const sumAge = selectedUsers.reduce((sum, item) => sum + item.age, 0);
   
-}
+  return selectedUsers.length ? sumAge / selectedUsers.length : 0;
+};
